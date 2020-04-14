6f5f1507fa91ac3cedebd8dcfaffb6bafc395642
uranesu <ai_g_c_vs@yahoo.co.jp>
2020-04-15 02:50:47 +0900

    josontest

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 "use strict";
 
+const start = () => {
+    alert("test");
+    return;
+}
+
 document.getElementById("start").addEventListener("click", start, false);
 
 $.getJSON("./resources/wores.json", function (data) {
     console.log(data);
 });
 
-const start = () => {
-    alert("test");
-    return;
-}
