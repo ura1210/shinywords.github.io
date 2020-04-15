843d8e99be925fa4d4ad76d87f7dade04f004dc9
uranesu <ai_g_c_vs@yahoo.co.jp>
2020-04-15 19:55:56 +0900

    dev

--- js/index.js
+++ js/index.js
@@ -7,8 +7,8 @@ class shinywords{
         sw.init();
         document.getElementById("start").style.display = "none";
         $.getJSON("./resources/wores.json", function (data) {
-            console.log(data.words[0][0]);
-            document.getElementById("words").textContent = data.words[0][0];
+            console.log(data[0][0]);
+            document.getElementById("words").textContent = data[0][0];
         });
        // alert(data[0]);
     }
