5ce4520d3443640c1afcfbab4dc60decfdd23265
uranesu <ai_g_c_vs@yahoo.co.jp>
2020-04-15 11:29:18 +0900

    josontest

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 "use strict";
-
-const start = () => {
-    alert("test");
-    return;
+class shinywords{
+    static start(){
+        document.getElementById("start").style.display = "none";
+        document.getElementById("words").style.display = "block";
+        document.getElementById("words").textContent = "start";
+        $.getJSON("./resources/wores.json", function (data) {
+            alert(data);
+        });
+        return;
+    }
 }
-
-document.getElementById("start").addEventListener("click", start, false);
-
-$.getJSON("./resources/wores.json", function (data) {
-    console.log(data);
-});
-
+document.getElementById("start").addEventListener("click", shinywords.start, false);
