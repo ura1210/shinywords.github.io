3da7f37a6b5720ffee9bea07280091c351b6d93e
uranesu <ai_g_c_vs@yahoo.co.jp>
2020-04-15 16:09:23 +0900

    dev

--- js/index.js
+++ js/index.js
@@ -1,13 +1,29 @@
 "use strict";
 class shinywords{
+    counter = 0;
     static start(){
         document.getElementById("start").style.display = "none";
         document.getElementById("words").style.display = "block";
         document.getElementById("words").textContent = "startserif";
         $.getJSON("./resources/wores.json", function (data) {
-            alert(data);
+            alert(data.mano);
         });
         return;
     }
+    static answer(idol){
+        counter++;
+        
+    }
+    static correctness(){
+        document.getElementById(`resultbox${counter}`).attributes.src = "";
+    }
+
 }
+
+function getID(element) {
+    const id = element.id;
+    alert(id);
+}
+
 document.getElementById("start").addEventListener("click", shinywords.start, false);
+document.getElementsByClassName("idolicon").addEventListener("click", shinywords.getID(this), false);
