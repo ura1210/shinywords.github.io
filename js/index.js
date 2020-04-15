f6647e15c122a43dc5802699c496002b8119c2b7
uranesu <ai_g_c_vs@yahoo.co.jp>
2020-04-15 20:52:33 +0900

    dev

--- js/index.js
+++ js/index.js
@@ -1,53 +1,106 @@
 "use strict";
-class shinywords{
-    static counter = 1;
+class shinywords {
+    counter = 1;
     correctness = "";
-    static start(){
+    static start() {
         const sw = new shinywords();
         sw.init();
         document.getElementById("start").style.display = "none";
-        $.getJSON("./resources/wores.json", function (data) {
-            console.log(data);
-            document.getElementById("words").textContent = data[0];
-        });
-       // alert(data[0]);
+        createQuiz();
+
     }
-    answer(idol){
+    answer(idol) {
         this.counter++;
-        if(correctness === idol ){
-            document.getElementById(`resultbox${this.counter}`).setAttribute('src', './pic/marubatu/Heart.png');           
-        }else{
-            document.getElementById(`resultbox${this.counter}`).setAttribute('src', './pic/marubatu/batu.png'); 
+        if (correctness === idol) {
+            document.getElementById(`resultbox${this.counter}`).setAttribute('src', './pic/marubatu/Heart.png');
+        } else {
+            document.getElementById(`resultbox${this.counter}`).setAttribute('src', './pic/marubatu/batu.png');
         }
     }
-    static correctness(){
-    }
     init() {
-        document.getElementById("mano").addEventListener("click",function(){this.answer(this)}, false);
-        document.getElementById("hiori").addEventListener("click",function(){this.answer(this)}, false);
-        document.getElementById("meguru").addEventListener("click",function(){this.answer(this)}, false);
-        document.getElementById("kogane").addEventListener("click",function(){this.answer(this)}, false);
-        document.getElementById("mamimi").addEventListener("click",function(){this.answer(this)}, false);
-        document.getElementById("sakuya").addEventListener("click",function(){this.answer(this)}, false);
-        document.getElementById("yuika").addEventListener("click",function(){this.answer(this)}, false);
-        document.getElementById("kiriko").addEventListener("click",function(){this.answer(this)}, false);
-        document.getElementById("amana").addEventListener("click",function(){this.answer(this)}, false);
-        document.getElementById("tenka").addEventListener("click",function(){this.answer(this)}, false);
-        document.getElementById("tiyuki").addEventListener("click",function(){this.answer(this)}, false);
-        document.getElementById("kaho").addEventListener("click",function(){this.answer(this)}, false);
-        document.getElementById("tiyoko").addEventListener("click",function(){this.answer(this)}, false);
-        document.getElementById("juri").addEventListener("click",function(){this.answer(this)}, false);
-        document.getElementById("rinze").addEventListener("click",function(){this.answer(this)}, false);
-        document.getElementById("natsuha").addEventListener("click",function(){this.answer(this)}, false);
-        document.getElementById("asahi").addEventListener("click",function(){this.answer(this)}, false);
-        document.getElementById("fuyuko").addEventListener("click",function(){this.answer(this)}, false);
-        document.getElementById("mei").addEventListener("click",function(){this.answer(this)}, false);
-        document.getElementById("tooru").addEventListener("click",function(){this.answer(this)}, false);
-        document.getElementById("madoka").addEventListener("click",function(){this.answer(this)}, false);
-        document.getElementById("koito").addEventListener("click",function(){this.answer(this)}, false);
-        document.getElementById("hinana").addEventListener("click",function(){this.answer(this)}, false);
+        document.getElementById("mano").addEventListener("click", function () { this.answer(this) }, false);
+        document.getElementById("hiori").addEventListener("click", function () { this.answer(this) }, false);
+        document.getElementById("meguru").addEventListener("click", function () { this.answer(this) }, false);
+        document.getElementById("kogane").addEventListener("click", function () { this.answer(this) }, false);
+        document.getElementById("mamimi").addEventListener("click", function () { this.answer(this) }, false);
+        document.getElementById("sakuya").addEventListener("click", function () { this.answer(this) }, false);
+        document.getElementById("yuika").addEventListener("click", function () { this.answer(this) }, false);
+        document.getElementById("kiriko").addEventListener("click", function () { this.answer(this) }, false);
+        document.getElementById("amana").addEventListener("click", function () { this.answer(this) }, false);
+        document.getElementById("tenka").addEventListener("click", function () { this.answer(this) }, false);
+        document.getElementById("tiyuki").addEventListener("click", function () { this.answer(this) }, false);
+        document.getElementById("kaho").addEventListener("click", function () { this.answer(this) }, false);
+        document.getElementById("tiyoko").addEventListener("click", function () { this.answer(this) }, false);
+        document.getElementById("juri").addEventListener("click", function () { this.answer(this) }, false);
+        document.getElementById("rinze").addEventListener("click", function () { this.answer(this) }, false);
+        document.getElementById("natsuha").addEventListener("click", function () { this.answer(this) }, false);
+        document.getElementById("asahi").addEventListener("click", function () { this.answer(this) }, false);
+        document.getElementById("fuyuko").addEventListener("click", function () { this.answer(this) }, false);
+        document.getElementById("mei").addEventListener("click", function () { this.answer(this) }, false);
+        document.getElementById("tooru").addEventListener("click", function () { this.answer(this) }, false);
+        document.getElementById("madoka").addEventListener("click", function () { this.answer(this) }, false);
+        document.getElementById("koito").addEventListener("click", function () { this.answer(this) }, false);
+        document.getElementById("hinana").addEventListener("click", function () { this.answer(this) }, false);
+    }
+
+
+    createQuiz() {
+        $.getJSON("./resources/wores.json", function (data) {
+            const num = getRandomInt(23);
+        switch (num) {
+            case 0:
+                document.getElementById("words").textContent = data.mano[getRandomInt(data.mano.length)];
+            case 1:
+                document.getElementById("words").textContent = data.mano[getRandomInt(data.mano.length)];
+            case 2:
+                document.getElementById("words").textContent = data.mano[getRandomInt(data.mano.length)];
+            case 3:
+                document.getElementById("words").textContent = data.mano[getRandomInt(data.mano.length)];
+            case 4:
+                document.getElementById("words").textContent = data.mano[getRandomInt(data.mano.length)];
+            case 5:
+                document.getElementById("words").textContent = data.mano[getRandomInt(data.mano.length)];
+            case 6:
+                document.getElementById("words").textContent = data.mano[getRandomInt(data.mano.length)];
+            case 7:
+                document.getElementById("words").textContent = data.mano[getRandomInt(data.mano.length)];
+            case 8:
+                document.getElementById("words").textContent = data.mano[getRandomInt(data.mano.length)];
+            case 9:
+                document.getElementById("words").textContent = data.mano[getRandomInt(data.mano.length)];
+            case 10:
+                document.getElementById("words").textContent = data.mano[getRandomInt(data.mano.length)];
+            case 11:
+                document.getElementById("words").textContent = data.mano[getRandomInt(data.mano.length)];
+            case 12:
+                document.getElementById("words").textContent = data.mano[getRandomInt(data.mano.length)];
+            case 13:
+                document.getElementById("words").textContent = data.mano[getRandomInt(data.mano.length)];
+            case 14:
+                document.getElementById("words").textContent = data.mano[getRandomInt(data.mano.length)];
+            case 15:
+                document.getElementById("words").textContent = data.mano[getRandomInt(data.mano.length)];
+            case 16:
+                document.getElementById("words").textContent = data.mano[getRandomInt(data.mano.length)];
+            case 17:
+                document.getElementById("words").textContent = data.mano[getRandomInt(data.mano.length)];
+            case 18:
+                document.getElementById("words").textContent = data.mano[getRandomInt(data.mano.length)];
+            case 19:
+                document.getElementById("words").textContent = data.mano[getRandomInt(data.mano.length)];
+            case 20:
+                document.getElementById("words").textContent = data.mano[getRandomInt(data.mano.length)];
+            case 21:
+                document.getElementById("words").textContent = data.mano[getRandomInt(data.mano.length)];
+            case 22:
+                document.getElementById("words").textContent = data.mano[getRandomInt(data.mano.length)];
+        }
+         });
     }
 }
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
 document.getElementById("start").addEventListener("click", shinywords.start, false);
